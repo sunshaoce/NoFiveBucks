feb638cd7c459dfa25b956db415069db64efbd8f
Shao-Ce SUN <sunshaoce@outlook.com>
2024-11-08 16:13:18 +0800

    update

--- double_click_close_tab/content.js
+++ double_click_close_tab/content.js
@@ -2,7 +2,7 @@ document.addEventListener("dblclick", (event) => {
   const excludeElements = ['INPUT', 'TEXTAREA', 'BUTTON', 'SELECT', 'A', 'LABEL'];
 
   if (
-    event.target.tagName === 'DIV' &&
+    (event.target.tagName === 'DIV' || event.target.tagName === 'BODY') &&
     !excludeElements.includes(event.target.tagName)
   ) {
     chrome.runtime.sendMessage({ action: "closeTab" });
